29d896a7fb94fac1767758ec56b082e9f97ed086
Ana Scorsim <albscorsim@gmail.com>
2023-07-06 15:02:41 -0300

    add dark mode image

--- script.js
+++ script.js
@@ -8,4 +8,16 @@ function toggleMode() {
   } else {
     html.classList.add('light')
   } */
+
+  // pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  // substituir a imagem
+  if (html.classList.contains("light")) {
+    // se tiver light mode adicionar a imagem light
+    img.setAttribute("src", "./assets/avatar-light.png")
+  } else {
+    // se tiver sem light mode manter a img normal
+    img.setAttribute("src", "./assets/avatar.png")
+  }
 }
